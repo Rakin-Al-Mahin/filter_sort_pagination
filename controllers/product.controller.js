2ab3886c251b61dfbde5f500b987f9f22b3b278e
Mahin <rakin99raj@gmail.com>
2024-06-27 00:09:25 +0600

    respocne

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -4,7 +4,7 @@ const Product = require("../models/product.model");
 const createProduct = async (req, res) => {
   try {
     const product = await Product.create(req.body);
-    res.status(200).json(product);
+    res.status(200).json({ message: "Product created successfully", product });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -42,6 +42,9 @@ const getProduct = async (req, res) => {
   try {
     const { id } = req.params;
     const product = await Product.findById(id);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
     res.status(200).json(product);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -53,14 +56,15 @@ const updateProduct = async (req, res) => {
   try {
     const { id } = req.params;
 
-    const product = await Product.findByIdAndUpdate(id, req.body);
+    const product = await Product.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
 
     if (!product) {
       return res.status(404).json({ message: "Product not found" });
     }
 
-    const updatedProduct = await Product.findById(id);
-    res.status(200).json(updatedProduct);
+    res.status(200).json({ message: "Product updated successfully", product });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
